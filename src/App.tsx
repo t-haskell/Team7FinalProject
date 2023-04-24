47fc931485538fbf0aff53634158928c40170c3c
Anthony <aandujar@udel.edu>
2023-04-23 21:56:22 -0400

    Overrided everything and fixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,72 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import adidas from "../src/images/1.png";
+import nike from "../src/images/2.png";
+import puma from "../src/images/3.png";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                <img
-                    src="/images/solematelogo.jpg"
-                    alt="SoleMate Main Logo"
-                ></img>
-                Team 7 Final Project --- SoleMate
-            </header>
-            <p>we ball</p>
-            <ul>
-                <li>Anthony Andujar</li>
-                <li>Tommy Haskell</li>
-                <li>Jadyn Worthington</li>
-                <li>Tabetha Chubb</li>
-                <li>Greg Mensah</li>
-                <li>Joey Blair</li>
-            </ul>
-            <hr></hr>
-            Choose your User level.
-            <hr></hr>
-            <Button>User</Button>
-            ADD ON CLICK - MAIN PAGE
-            <p></p>
-            <Button>Admin</Button>
-            ADD ON CLICK - PASSWORD PAGE
-            <p></p>
-            <Button>Super</Button>
-            ADD ON CLICK - PASSWORD PAGE
+            <head>
+                <title>Navpage mockup</title>
+            </head>
+            <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="">Bucket?</a>
+                        </li>
+                        <li>
+                            <a href="">Bucket?</a>
+                        </li>
+                    </ul>
+                </nav>
+                <header>
+                    <div className="bg">
+                        <div className="content">
+                            <div className="kolom">
+                                <div className="a">
+                                    <img src={adidas} />
+                                </div>
+                                <div className="b">
+                                    <a href="#adidas">ADIDAS</a>
+                                </div>
+                            </div>
+                            <div className="kolom">
+                                <div className="a">
+                                    <img src={nike} />
+                                </div>
+                                <div className="b">
+                                    <a href="#nike">NIKE</a>
+                                </div>
+                            </div>
+                            <div className="kolom">
+                                <div className="a">
+                                    <img src={puma} />
+                                </div>
+                                <div className="b">
+                                    <a href="#puma">PUMA</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <footer className="ADIDAS">
+                    <div id="adidas">
+                        <h1>ADIDAS</h1>
+                    </div>
+                </footer>
+                <footer className="NIKE">
+                    <div id="nike"></div>
+                    <h1>NIKE</h1>
+                </footer>
+                <footer className="PUMA">
+                    <div id="puma"></div>
+                    <h1>PUMA</h1>
+                </footer>
+            </body>
         </div>
     );
 }
